ef19e8b4f3f167b89ffd0fe0f0e5f64d8db4c062
Anna Samsonova <samsonova1095@mail.ru>
2023-08-06 14:29:02 +0300

    My unit tests

---
+++ task_1.java
@@ -0,0 +1,6 @@
+@Test
+public void checkIsAdultWhenAgeIsMoreThan18True() {
+    Program program = new Program();
+    boolean isAdult = program.checkIsAdult(19);
+	assertEquals("Метод возвращает true если >=18", true, isAdult);
+    }
